6485862fe1549c7348d153a41613b3d381e0c56c
Cristina Edens <edens.cristina@gmail.com>
2019-12-10 22:24:04 -0500

    fixed path and form submit

--- php/submit-form.php
+++ php/submit-form.php
@@ -1,27 +1,10 @@
-<?PHP
-  // form handler
-  if($_POST && isset( $_POST['name'], $_POST['email'], $_POST['subject'], $_POST['message'])) {
-
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
-
-    if(!$name) {
-      $errorMsg = "Please enter your Name";
-    } elseif(!$email || !preg_match("/^\S+@\S+$/", $email)) {
-      $errorMsg = "Please enter a valid Email address";
-    } elseif(!$message) {
-      $errorMsg = "Please enter your comment in the Message box";
-    } else {
-      // send email and redirect
-      $to = "edens.cristina@gmail.com";
-      if(!$subject) $subject = "Contact from website";
-      $headers = "From: noreply@noreply.com" . "\r\n";
-      mail($to, $subject, $message, $headers);
-      header("Location: https://crstinaedens.com");
-      exit;
-    }
-
-  }
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "edens.cristina@gmail.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
 ?>
